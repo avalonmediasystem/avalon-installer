4d9db622c667ad5bc50bb02573dad327ae9ef55d
Adam Hallett <adam.t.hallett@gmail.com>
2013-03-14 15:11:26 -0500

    first push, can i get some feedback?

--- manifests/avalon/framework.pp
+++ manifests/avalon/framework.pp
@@ -2,9 +2,9 @@ class avalon::framework {
   include epel
   include nulrepo
   include mediainfo
+  include rvm
 
-  package { ["curl", "gcc-c++", "sqlite", "v8-devel", 
-    "libxml2-devel", "libxslt-devel", "make", "zip"]:
+  package { ["curl", "sqlite", "v8-devel", "zip"]:
     ensure => present
   }
   #todo parameterize everything that follows.
@@ -13,29 +13,25 @@ class avalon::framework {
     ensure => present
   }
 
-  rbenv::install { "vagrant":
-    group => "vagrant",
-    home  => "/home/vagrant"
+  rvm::system_user { vagrant: ; }
+
+  rvm_system_ruby {
+  'ruby-1.9.3-p392':
+    ensure => 'present',
+    default_use => true
   }
 
-  rbenv::compile { "1.9.3-p392":
-    user => "vagrant",
-    home => "/home/vagrant",
-    global => true,
-    source => "puppet:///local/ruby-def",
-    require => Rbenv::Install["vagrant"]
+  rvm_gemset {
+    'ruby-1.9.3-p392@avalon':
+    ensure => present,
+    require => Rvm_system_ruby['ruby-1.9.3-p392']
   }
-  ##TODO Parameterize at worst, be more clever
-  exec { 'passenger_gem_install':
-  user    => 'vagrant',
-  command => 'gem install passenger',
-  unless  => 'gem list passenger | grep passenger',
-  path    => ['/home/vagrant/.rbenv/shims',
-              '/home/vagrant/.rbenv/bin',
-              '/usr/local/bin',
-              '/bin', '/usr/bin', '/usr/local/sbin',
-              '/usr/sbin','/sbin','/home/vagrant/bin',],
-  require => Rbenv::Compile ["1.9.3-p392"],
+
+
+  rvm_gem {
+  'ruby-1.9.3-p392@avalon/bundler':
+    ensure => '~> 1.2.3',
+    require => Rvm_gemset['ruby-1.9.3-p392@avalon']
   }
 
 }
