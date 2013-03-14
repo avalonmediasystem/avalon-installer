1c9f9daa816988a4d3ae8af5b5167d34fd6753c3
Adam Hallett <adam.t.hallett@gmail.com>
2013-03-14 16:04:18 -0500

    style changes

--- manifests/avalon/framework.pp
+++ manifests/avalon/framework.pp
@@ -1,37 +1,38 @@
+# Install and configure the Avalon application
 class avalon::framework {
   include epel
   include nulrepo
   include mediainfo
   include rvm
 
-  package { ["curl", "sqlite", "v8-devel", "zip"]:
+  package { ['curl', 'sqlite', 'v8-devel', 'zip']:
     ensure => present
   }
   #todo parameterize everything that follows.
-  file { "/home/vagrant/.bash_profile":
-    source => "puppet:///local/bash_profile",
-    ensure => present
+  file { '/home/vagrant/.bash_profile':
+    ensure => present,
+    source => 'puppet:///local/bash_profile',
   }
 
   rvm::system_user { vagrant: ; }
 
   rvm_system_ruby {
   'ruby-1.9.3-p392':
-    ensure => 'present',
-    default_use => true
+    ensure      => 'present',
+    default_use => true,
   }
 
   rvm_gemset {
     'ruby-1.9.3-p392@avalon':
-    ensure => present,
-    require => Rvm_system_ruby['ruby-1.9.3-p392']
+    ensure  => present,
+    require => Rvm_system_ruby['ruby-1.9.3-p392'],
   }
 
 
   rvm_gem {
   'ruby-1.9.3-p392@avalon/bundler':
-    ensure => '~> 1.2.3',
-    require => Rvm_gemset['ruby-1.9.3-p392@avalon']
+    ensure  => '~> 1.2.3',
+    require => Rvm_gemset['ruby-1.9.3-p392@avalon'],
   }
 
 }
