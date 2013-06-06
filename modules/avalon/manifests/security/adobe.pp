d374536047a59226c19b63410a9163d081847154
Michael Klein <mbklein@gmail.com>
2013-06-06 09:44:51 -0500

    Set correct stream path for AMS

--- modules/avalon/manifests/security/adobe.pp
+++ modules/avalon/manifests/security/adobe.pp
@@ -19,7 +19,7 @@ class avalon::security::adobe {
   }
 
   exec { "add Avalon config to ams.ini":
-    command => "/usr/bin/printf \"\n\nAVALON.AUTH_URL = ${avalon::info::avalon_url}/authorize\nAVALON.STREAM_PATH = /opt/adobe/ams/webroot/avalon\n\" >> ams.ini",
+    command => "/usr/bin/printf \"\n\nAVALON.AUTH_URL = ${avalon::info::avalon_url}/authorize\nAVALON.STREAM_PATH = ${avalon::info::root_dir}/rtmp_streams\n\" >> ams.ini",
     cwd     => '/opt/adobe/ams/conf',
     unless  => "/bin/grep '${avalon::info::avalon_url}/authorize' ams.ini",
     notify  => Service['ams']
@@ -37,11 +37,5 @@ class avalon::security::adobe {
     unless  => "/bin/grep avalon.conf httpd.conf",
     require => File['/opt/adobe/ams/Apache2.2/conf/avalon.conf'],
     notify  => Service['ams']
-  }
-
-  file { '/opt/adobe/ams/webroot/avalon':
-    ensure  => link,
-    force   => true,
-    target  => "${avalon::info::root_dir}/rtmp_streams"
   }
 }
