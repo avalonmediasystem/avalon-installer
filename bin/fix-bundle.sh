aff2ada3eb96872ee4a29b9ddd82f7ac7d404a8b
Michael Klein <mbklein@gmail.com>
2015-09-23 09:48:29 -0500

    Clean up Gemfiles

--- bin/fix-bundle.sh
+++ bin/fix-bundle.sh
@@ -5,3 +5,4 @@ wget https://raw.githubusercontent.com/avalonmediasystem/avalon/${1}/Gemfile
 wget https://raw.githubusercontent.com/avalonmediasystem/avalon/${1}/Gemfile.lock 
 ln -s /var/www/avalon/shared/Gemfile.local .
 /usr/local/rvm/bin/rvm-shell 'default' -c 'bundle install --path /var/www/avalon/shared/bundle --path=/var/www/avalon/shared/gems --without development test'
+rm Gemfile Gemfile.lock Gemfile.local
