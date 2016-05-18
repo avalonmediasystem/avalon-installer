9138b92156d9846877bac89bd8b83445435b4fa3
Phuong Dinh <phuongdh@gmail.com>
2016-05-18 10:47:21 -0400

    Up ruby version and build off develop branch

--- modules/avalon/manifests/params.pp
+++ modules/avalon/manifests/params.pp
@@ -1,20 +1,20 @@
 # Copyright 2011-2013, The Trustees of Indiana University and Northwestern
 #   University.  Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
-# 
+#
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software distributed 
+#
+# Unless required by applicable law or agreed to in writing, software distributed
 #   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-#   CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
 #   specific language governing permissions and limitations under the License.
 # ---  END LICENSE_HEADER BLOCK  ---
 
 class avalon::params {
   $ruby_version           = $rvm_latest_ruby
-  $source_branch          = "master"
+  $source_branch          = "develop"
   $deploy_tag             = "bare-deploy"
   $dropbox_user           = $avalon_dropbox_user
   $dropbox_password_hash  = $avalon_dropbox_password_hash
